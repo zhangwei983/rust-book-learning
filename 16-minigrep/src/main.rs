9e58fd9e3e292b878f968f307fee638a6d5cb80e
Vincent Zhang <zhangwei983@hotmail.com>
2024-05-01 17:44:14 +0800

    Add mingrep examples to show how to access arguments, environment viariables, file IO etc..

--- 16-minigrep/src/main.rs
+++ 16-minigrep/src/main.rs
@@ -1,3 +1,20 @@
+use std::{env, process};
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // dbg!(&args);
+
+    let config = Config::build(&args).unwrap_or_else(|error| {
+        eprintln!("Failed parsing arguments: {error}");
+        process::exit(1);
+    });
+
+    // println!("Search for '{}'", config.query);
+    // println!("In file '{}'", config.file_path);
+
+    if let Err(err) = minigrep::run(config) { // Here the config will be moved.
+        eprintln!("Application error: {err}");
+        process::exit(1);
+    }
 }
