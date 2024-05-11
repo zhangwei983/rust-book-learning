e49e01413482a1391310bd1af7e8b670a4c538c0
Vincent Zhang <zhangwei983@hotmail.com>
2024-05-11 14:15:53 +0800

    Add dummy code for message passing.

--- 20-concurrency/src/main.rs
+++ 20-concurrency/src/main.rs
@@ -1,5 +1,9 @@
 mod thread_spawn;
+mod message_passing;
 
 fn main() {
     thread_spawn::test_thread_spawn();
+    println!("");
+
+    message_passing::test_message_passing();
 }
---
+++ 20-concurrency/src/message_passing.rs
@@ -0,0 +1,7 @@
+pub fn test_message_passing() {
+    println!("--- Start module: {}", module_path!());
+
+    
+
+    println!("--- End module: {}", module_path!());
+}
