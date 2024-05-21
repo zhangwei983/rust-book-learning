c0e68cd487257fc9aee22d9ee6a363b9718828ab
Vincent Zhang <zhangwei983@hotmail.com>
2024-05-21 22:34:37 +0800

    Add examples for pattern syntax.

--- 23-pattern_matching/README.md
+++ 23-pattern_matching/README.md
@@ -1,2 +1,216 @@
 An example to show how pattern matching works in Rust.
 
+## Pattern Matching
+
+### Basic Pattern Matching
+
+A typical match pattern using `match` expression with multiple arms.
+
+```rust
+match x {
+    None => None,
+    Some(i) => Some(i + 1),
+}
+```
+
+### Conditional `if let`
+
+Use `if let` to match for one case.
+
+```rust
+if let Some(i) = x {
+    println!("x: {}", i);
+}
+```
+
+### Conditional Loop `while let`
+
+Use `while let` to loop as long as the pattern continues to match.
+
+```rust
+let mut stack = Vec::new();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+while let Some(top) = stack.pop() {
+    println!("{}", top);
+}
+```
+
+### for Loops
+
+The value that directly follows the keyword for is a pattern.
+
+```rust
+let v = vec!['a', 'b', 'c'];
+
+for (index, value) in v.iter().enumerate() {
+    println!("{} is at index {}", value, index);
+}
+```
+
+`(index, value)` is the pattern in the above example.
+
+### let Statements
+
+```rust
+let PATTERN = EXPRESSION;
+```
+
+Every time you've used a `let` statement like this you've been using patterns, although you might not have realized it.
+
+```rust
+let (x, y, z) = (1, 2, 3);
+```
+
+### Function Parameters
+
+The function parameter can also be a pattern.
+
+```rust
+fn foo(x: i32) {
+    // code goes here
+}
+```
+
+The `x` part is a pattern! 
+
+## Refutability
+
+Patterns come in two forms: `refutable` and `irrefutable`. Please check the example [here](./src/refutability.rs).
+
+### Irrefutable
+
+Patterns that will match for any possible value passed are `irrefutable`. 
+
+The `let` statements are irrefutable. For example, `let x =5`, `x` matches anything and therefore cannot fail to match.
+
+`Function parameters`, `let` statements, and `for` loops can only accept irrefutable patterns, because the program cannot do anything meaningful when values don’t match. 
+
+### Refutable
+
+Patterns that can fail to match for some possible value are refutable. 
+
+For example, `if let Some(x) = a_value`, `Some(x)` is refutable as the value in the `a_value` variable might be `None`.
+
+The `if let` and `while let` expressions accept `refutable` and `irrefutable` patterns, but the compiler warns against irrefutable patterns.
+
+
+## Pattern Syntax
+
+Please check [this example](./src/pattern_syntax.rs) for pattern syntax.
+
+### Basic Matching
+
+Match multiple patterns.
+
+```rust
+let x = 1;
+match x {
+    1 | 2 => println!("One or two"),
+    _ => println!("Other"),
+}
+```
+
+Match range.
+
+```rust
+let x = 5;
+match x {
+    1..=5 => println!("One to five"),
+    _ => println!("Other"),  
+}
+```
+
+### Destructure Structs
+
+```rust
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+let p = Point { x: 0, y: 7 };
+
+let Point { x: a, y: b } = p;
+assert_eq!(0, a);
+assert_eq!(7, b);
+```
+
+Creates the variables `a` and `b` that match the values of the x and y fields of the point struct.
+
+### Destructure Enums
+
+```rust
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+let msg = Message::ChangeColor(0, 160, 255);
+
+match msg {
+    Message::Quit => {
+        println!("The Quit variant has no data to destructure.");
+    }
+    Message::Move { x, y } => {
+        println!("Move in the x direction {x} and in the y direction {y}");
+    }
+    Message::Write(text) => {
+        println!("Text message: {text}");
+    }
+    Message::ChangeColor(r, g, b) => {
+        println!("Change the color to red {r}, green {g}, and blue {b}",)
+    }
+}
+```
+
+We can destructure enums to retrive the data stored on it.
+
+### Ignore Values
+
+There are a few ways to ignore entire values or parts of values in a pattern: 
+- using the `_` pattern
+- using the `_` pattern within another pattern
+- using a name that starts with an `underscore`, 
+- using `..` to ignore a certain range parts of a value.
+
+### Match Guards
+
+A match guard is an additional `if` condition, specified after the pattern in a match arm, that must also match for that arm to be chosen. 
+
+```rust
+let num = Some(4);
+
+match num {
+    Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+    Some(x) => println!("The number {} is odd", x),
+    None => (),
+}
+```
+
+The above match has its first arm with the pattern `Some(x)` and also a `match guard` of `if x % 2 == 0`.
+
+
+### Match Bindings
+
+Use the `at` operator `@` to create a variable that holds a value at the same time as we’re testing that value for a pattern match.
+
+```rust
+enum Message {
+        Hello { id: i32 },
+    }
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message::Hello { id } => println!("Found some other id: {}", id),
+    }
+```
--- 23-pattern_matching/src/pattern_syntax.rs
+++ 23-pattern_matching/src/pattern_syntax.rs
@@ -1,5 +1,190 @@
+fn test_basic_match() {
+    // Match literals.
+    let x = 1;
+    match x {
+        1 => println!("One"),
+        2 => println!("Two"),
+        _ => println!("Other"),
+    }
+
+    // Match named variables.
+    let x = Some(5);
+    let y = 10;
+    match x {
+        Some(50) => println!("Fifty"),
+        Some(y) => println!("x = {}", y), // This y hides the `let y`, and match x Some(5).
+        _ => println!("Other"),
+    }
+    println!("x = {:?}, y = {y}", x);
+
+    // Match multiple patterns
+    let x = 1;
+    match x {
+        1 | 2 => println!("One or two"),
+        _ => println!("Other"),
+    }
+
+    // Match range.
+    let x = 5;
+    match x {
+        1..=5 => println!("One to five"),
+        _ => println!("Other"),  
+    }
+
+    let ch = 'c';
+    match ch {
+        'a'..='j' => println!("a to j"),
+        'k'..='z' => println!("k to z"),
+        _ => println!("Other"),
+    }
+}
+
+fn test_destructure_structs() {
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let point = Point { x: 0, y: 7 };
+
+    // Creates the variables `a` and `b` that match the values of the x and y fields of the point struct.
+    let Point { x: a, y: b } = point;
+    println!("a: {}, b: {}", a, b);
+
+    // A shorter version.
+    let Point { x, y } = point;
+    println!("x: {}, y: {}", x, y);
+
+    // Destructure with partial literal values.
+    match point {
+        Point { x, y: 0 } => println!("On X axis: ({}, {})", x, y), // y has to match 0.
+        Point { x: 0, y} => println!("On Y axis: ({}, {})", x, y), // x has to match 0.
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+
+    // Destructure structs and tuples.
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+    assert_eq!(feet, 3);
+    assert_eq!(inches, 10);
+    assert_eq!(x, 3);
+    assert_eq!(y, -10);
+}
+
+fn test_destructure_enums() {
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32),
+    }
+    
+    enum Message {
+        Quit,                       // Contains no data.
+        Move { x: i32, y: i32 },    // Contains a struct
+        Write(String),              // Contain a tuple.
+        ChangeColor(Color),         // Contain a tuple.
+    }
+
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::Quit => println!("The Quit variant has no data to destructure."),
+        Message::Move { x, y } => println!("Move in the x direction {x} and in the y direction {y}"),
+        Message::Write (str) => println!("Text message: {str}"),
+        Message::ChangeColor(Color::Rgb(r, g, b)) => println!("Change the color to r {r}, g {g}, and b {b}"),
+        Message::ChangeColor(Color::Hsv(h, s, v)) => println!("Change the color to h {}, s {}, v {}", h, s, v),
+    }
+}
+
+// Use the underscore as a wildcard pattern that will match any value but not bind to the value.
+fn foo(_: i32, y: i32) {
+    println!("Only uses the y parameter: {}", y);
+}
+
+fn test_ignore_values() {
+    foo(3, 4);
+
+    let mut value = Some(5);
+    let new_value = Some(10);
+
+    // Ingore partially.
+    match (value, new_value) {
+        (Some(_), _) => {
+            println!("Can't overwrite an existing value");
+        }
+        _ => {
+            value = new_value;
+        }
+    }
+
+    println!("Value is {:?}", value);
+
+    // Ignore partially from a tuple.
+    let numbers = (1, 2, 3, 4, 5);
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("{}, {}, {}", first, third, fifth);
+        }
+    }
+
+    // Ignore a range part with `..`.
+    match numbers {
+        (first, .., last) => {
+            println!("First is: {}, last is: {}", first, last);
+        }
+    }
+}
+
+fn test_match_guards() {
+    let number = Some(4);
+
+    // Use additional if as a match guard.
+    match number {
+        Some(x) if x % 2 == 0 => println!("{} is even", x),
+        Some(x) => println!("{} is odd", x),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    // Use additional if as a match guard.
+    match x {
+        Some(n) if n == y => println!("x matched y"),
+        _ => println!("x didn't match y"),
+    }
+}
+
+fn test_match_bindings() {
+    enum Message {
+        Hello { id: i32 },
+    }
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        // Binding id to id_var only if id is in range `3..=7`.
+        Message::Hello {
+            id: id_var @ 3..=7
+        } => println!("Id in range 3-7: {}", id_var),
+        // Only check if id is in range `8..=10` without binding it to a variable, so we cannot use it's value.
+        Message::Hello { id: 8..=10 } => println!("Id in range 8-10."),
+        Message::Hello { id } => println!("Id in other range: {}", id),
+    }
+}
+
 pub fn test_pattern_syntax() {
     println!("--- Start module: {}", module_path!());
+
+    test_basic_match();
+
+    test_destructure_structs();
+
+    test_destructure_enums();
+
+    test_ignore_values();
+
+    test_match_guards();
+
+    test_match_bindings();
 
     println!("--- End module: {}", module_path!());
 }
