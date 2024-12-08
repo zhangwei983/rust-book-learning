e9b08624423df30dca74e24eb29dd44f4f407925
Vincent Zhang <zhangwei983@hotmail.com>
2024-12-08 16:04:49 +0800

    Add serde example for default values.

---
+++ 32-serialization/serde_examples/src/default_values.rs
@@ -0,0 +1,68 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Request {
+    // If the value is not present when deserializing, call a function to get a default value.
+    #[serde(default = "default_resource")]
+    resource: String,
+
+    // If the value is not present when deserializing, use the Default::default().
+    #[serde(default)]
+    timeout: Timeout,
+
+    // If the value is not present when deserializing, call a function to get a default value.
+    #[serde(default = "Priority::lowest")]
+    priority: Priority,
+}
+
+fn default_resource() -> String {
+    "/".to_string()
+}
+
+#[derive(Debug, Deserialize)]
+struct Timeout(u32);
+
+impl Default for Timeout {
+    fn default() -> Self {
+        Timeout(30)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+enum Priority {
+    ExtraHigh,
+    High,
+    Normal,
+    Low,
+    ExtraLow,
+}
+
+impl Priority {
+    fn lowest() -> Self {
+        Priority::ExtraLow
+    }
+}
+
+pub fn test() {
+    println!("--- Start module: {}", module_path!());
+
+    // r# for raw string in Rust.
+    // https://doc.rust-lang.org/reference/tokens.html#raw-string-literals
+    let json_string = r#"
+        [
+            {
+                "resource": "/users"
+            },
+            {
+                "timeout": 5,
+                "priority": "High"
+            }
+        ]
+    "#;
+
+    let requests: Vec<Request> = serde_json::from_str(json_string).unwrap();
+    println!("Request 0 : {:?}", requests[0]);
+    println!("Request 1 : {:?}", requests[1]);
+
+    println!("--- End module: {}", module_path!());
+}
--- 32-serialization/serde_examples/src/main.rs
+++ 32-serialization/serde_examples/src/main.rs
@@ -1,8 +1,11 @@
 mod attributes;
 mod basic;
+mod default_values;
 
 fn main() {
     basic::test();
     println!("");
     attributes::test();
+    println!("");
+    default_values::test();
 }
