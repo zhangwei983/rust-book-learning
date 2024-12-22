9f1ad76806220e4ab0d5c1c465d01526081a2802
Vincent Zhang <zhangwei983@hotmail.com>
2024-12-22 21:54:58 +0800

    Add a basic sample of slog.

---
+++ 33-logging/slog_example/src/log_to_terminal.rs
@@ -0,0 +1,21 @@
+use chrono::Utc;
+use slog::{self, info, Drain};
+
+pub fn test() {
+    println!("--- Start module: {}", module_path!());
+
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    let root_logger = slog::Logger::root(drain, slog::o!());
+
+    info!(root_logger, "--- Start module: {}", module_path!());
+
+    info!(root_logger, "Module started";
+        "started_at" => format!("{}", Utc::now()));
+
+    info!(root_logger, "Module ended";
+        "ended_at" => format!("{}", Utc::now()));
+
+    info!(root_logger, "--- End module: {}", module_path!());
+}
---
+++ 33-logging/slog_example/src/main.rs
@@ -0,0 +1,5 @@
+mod log_to_terminal;
+
+fn main() {
+    log_to_terminal::test();
+}
