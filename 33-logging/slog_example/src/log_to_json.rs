24a3203e771074f1fdd49e90a48b53fe446d91b2
Vincent Zhang <zhangwei983@hotmail.com>
2024-12-23 09:21:31 +0800

    Add a sample for json.

---
+++ 33-logging/slog_example/src/log_to_json.rs
@@ -0,0 +1,14 @@
+use std::sync::Mutex;
+
+use chrono::Utc;
+use slog::{self, info, Drain};
+
+pub fn test() {
+    let drain = Mutex::new(slog_json::Json::default(std::io::stdout())).fuse();
+    let root_logger = slog::Logger::root(drain, slog::o!());
+
+    info!(root_logger, "--- Start module: {}", module_path!());
+    info!(root_logger, "Module started"; "started_at" => format!("{}", Utc::now()));
+    info!(root_logger, "Module ended"; "ended_at" => format!("{}", Utc::now()));
+    info!(root_logger, "--- End module: {}", module_path!());
+}
--- 33-logging/slog_example/src/main.rs
+++ 33-logging/slog_example/src/main.rs
@@ -1,4 +1,5 @@
 mod log_to_file;
+mod log_to_json;
 mod log_to_multiple_drains;
 mod log_to_terminal;
 mod logging_level;
@@ -11,4 +12,6 @@ fn main() {
     log_to_file::test();
     println!("");
     log_to_multiple_drains::test();
+    println!("");
+    log_to_json::test();
 }
