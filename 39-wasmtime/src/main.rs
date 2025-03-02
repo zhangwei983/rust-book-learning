88c1eb672f919f496866544fac88f9c581b9a852
Vincent Zhang <zhangwei983@hotmail.com>
2025-03-02 23:39:23 +0800

    Add a sample for linear memory usage.

--- 39-wasmtime/src/main.rs
+++ 39-wasmtime/src/main.rs
@@ -1,8 +1,11 @@
 mod basic_usage;
 mod import_host_func;
+mod linear_memory;
 
 fn main() {
     basic_usage::test();
     println!("");
     import_host_func::test();
+    println!("");
+    linear_memory::test();
 }
